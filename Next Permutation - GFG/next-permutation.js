ec8ae5d0fb8b0f6b75370259f595cc5bb791e0e4
Subham Rout <53035764+subham1rout@users.noreply.github.com>
2023-05-14 12:34:04 +0530

    Added solution - LeetHub

---
+++ Next Permutation - GFG/next-permutation.js
@@ -0,0 +1,99 @@
+//{ Driver Code Starts
+//Initial Template for javascript
+
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((string) => {
+      return string.trim();
+    });
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/* Function to print an array */
+function printArray(arr, size) {
+  let i;
+  let s = "";
+  for (i = 0; i < size; i++) {
+    s += arr[i] + " ";
+  }
+  console.log(s);
+}
+
+function main() {
+  let t = parseInt(readLine());
+  let i = 0;
+  for (; i < t; i++) {
+    let n = parseInt(readLine());
+    let arr = new Array(n);
+    let input_ar1 = readLine().split(" ").map((x) => parseInt(x));
+    for (let i = 0; i < n; i++) arr[i] = input_ar1[i];
+    let obj = new Solution();
+    let res = obj.nextPermutation(arr,n);
+    printArray(res,res.length);
+  }
+}
+// } Driver Code Ends
+
+
+//User function Template for javascript
+
+/**
+ * @param {Number[]} arr
+ * @param {Number} n
+ * @returns {Number[]}
+ */
+
+class Solution {
+    
+    reverse(arr,start,end){
+        while(start<end){
+            let temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+        return arr;
+    }
+    
+    nextPermutation(arr,n){
+        let index=-1;
+        for(let i=n-2;i>=0;i--){
+            if(arr[i]<arr[i+1]){
+                index=i;
+                break;
+            }
+        }
+        if(index==-1){
+            return this.reverse(arr,0,n-1);
+        }
+        for(let i=n-1;i>index;i--){
+            if(arr[i]>arr[index]){
+                let temp=arr[i];
+                arr[i]=arr[index];
+                arr[index]=temp;
+                break;
+            }
+        }
+        return this.reverse(arr,index+1,n-1);
+    }
+}
