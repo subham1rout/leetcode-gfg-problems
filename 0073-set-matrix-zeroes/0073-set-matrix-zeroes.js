ab8e5fe9f98ac485509d82e9f9b77e302edb02ab
Subham Rout <53035764+subham1rout@users.noreply.github.com>
2023-05-18 16:58:23 +0530

    Time: 69 ms (95.28%), Space: 44.1 MB (95.92%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
@@ -5,21 +5,28 @@
 var setZeroes = function(matrix) {
     let m=matrix.length;
     let n=matrix[0].length;
-    let rowarr=new Array(m).fill(0);
-    let colarr=new Array(n).fill(0);
+    let temp=1;
     for(let i=0;i<m;i++){
         for(let j=0;j<n;j++){
             if(matrix[i][j]==0){
-                rowarr[i]=1;
-                colarr[j]=1;
+                matrix[i][0]=0;
+                if(j==0){
+                    temp=0;
+                }else{  
+                    matrix[0][j]=0;
+                }
             }
         }
     }
-    for(let i=0;i<m;i++){
-        for(let j=0;j<n;j++){
-            if(rowarr[i] || colarr[j]){
-                matrix[i][j]=0;
+    for(let i=1;i<m;i++){
+        for(let j=1;j<n;j++){
+            if(matrix[i][j]!=0){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
+                    matrix[i][j]=0;
+                }
             }
         }
     }
+    if(matrix[0][0]==0){ for(let j=0;j<n;j++) matrix[0][j]=0 }
+    if(temp==0){ for(let i=0;i<m;i++) matrix[i][0]=0 }
 };
