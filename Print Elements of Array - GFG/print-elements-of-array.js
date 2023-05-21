660a0f6fd5f110c990ee62324630d9a2e9242d45
Subham Rout <53035764+subham1rout@users.noreply.github.com>
2023-05-21 23:21:47 +0530

    Added solution - LeetHub

---
+++ Print Elements of Array - GFG/print-elements-of-array.js
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+//Initial Template for javascript
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for(;i<t;i++)
+    {
+        let n = parseInt(readLine());
+        let arr = readLine().split(' ').map(x=>parseInt(x));
+        let obj = new Solution();
+        obj.printArray(arr, n);
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for javascript
+
+/**
+ * @param {number[]} arr
+ * @param {number} n
+ */
+class Solution {
+    printArray(arr, n) {
+        let string="";
+        for(let i=0;i<n;i++){
+            string+=arr[i]+" ";
+        }
+        console.log(string);
+    }
+}
