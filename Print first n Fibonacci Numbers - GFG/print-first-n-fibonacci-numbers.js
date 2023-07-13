ae10303107f347dcac2a73c38dc8e17356ba11de
Subham Rout <53035764+subham1rout@users.noreply.github.com>
2023-07-14 00:28:03 +0530

    Added solution - LeetHub

---
+++ Print first n Fibonacci Numbers - GFG/print-first-n-fibonacci-numbers.js
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+//Initial Template for javascript
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    let dp = new Array();
+    dp.push(BigInt(0));
+    dp.push(BigInt(1));
+    dp.push(BigInt(1));
+    for(let i=4;i<=100;i++)
+    {
+        dp.push(BigInt(0));
+    }
+    
+    let t = parseInt(readLine());
+    for(let i=0;i<t;i++)
+    {
+        let input_line = readLine().split();
+        let N = BigInt(input_line[0]);
+        let obj = new Solution();
+        let ans = obj.printFibb(N, dp);
+        let s = "";
+        for(let j=0;j<ans.length;j++)
+        {
+            s+=ans[j];
+            s+=" ";
+        }
+        console.log(s);
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for javascript
+
+/**
+ * @param {BigInt} N
+ * @returns {BigInt[]} 
+*/
+class Solution
+{
+    //Function to return list containing first n fibonacci numbers.
+    printFibb(N)
+    {
+        if(N==1n) return [1];
+        let fibseries=[1n,1n];
+        for(let i=2n;i<N;i++){
+            fibseries[i]=fibseries[i-1n]+fibseries[i-2n];
+        }
+        return fibseries;
+    }
+}
