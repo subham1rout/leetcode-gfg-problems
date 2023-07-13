da5af343f42fbe30f01c1fb6ba18e4810340fa2e
Subham Rout <53035764+subham1rout@users.noreply.github.com>
2023-07-14 00:09:56 +0530

    Time: 85 ms (36.25%), Space: 41.7 MB (67.95%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.js
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -3,9 +3,13 @@
  * @return {number}
  */
 var fib = function(n) {
-    let arr=[0,1];
-    for(let i=2;i<=n;i++){
-        arr[i]=arr[i-1]+arr[i-2];
-    }
-    return arr[n];
+    if(n<=1) return n;
+    return fib(n-1)+fib(n-2);
+
+    //using loop
+    // let arr=[0,1];
+    // for(let i=2;i<=n;i++){
+    //     arr[i]=arr[i-1]+arr[i-2];
+    // }
+    // return arr[n];
 };
