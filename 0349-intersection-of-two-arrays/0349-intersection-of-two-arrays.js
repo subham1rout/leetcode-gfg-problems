201b2f0ed2828a64c2c3f536123228721c5f127f
Subham Rout <53035764+subham1rout@users.noreply.github.com>
2023-08-24 17:07:07 +0530

    Time: 57 ms (63.88%), Space: 43.1 MB (59.07%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -6,14 +6,16 @@
 var intersection = function(nums1, nums2) {
     nums1=nums1.sort((a,b)=>a-b);
     nums2=nums2.sort((a,b)=>a-b);
-    let visited=new Array(nums2.length).fill(0);
+    let i=0;
+    let j=0;
     let result=[];
-    for(let i=0;i<nums1.length;i++){
-        for(let j=0;j<nums2.length;j++){
-            if(nums1[i]==nums2[j] && visited[j]==0 && !result.includes(nums1[i])){
-                result.push(nums1[i]);
-                visited[j]=1;
-            }
+    while(i<nums1.length && j<nums2.length){
+        if(nums1[i]<nums2[j]) i++;
+        else if(nums1[i]>nums2[j]) j++;
+        else{
+            if(!result.includes(nums1[i])) result.push(nums1[i]);
+            i++;
+            j++;
         }
     }
     return result;
