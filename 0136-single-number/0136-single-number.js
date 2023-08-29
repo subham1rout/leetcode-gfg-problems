92a4fb8208f6f1486f32937a43b9487ad69c7db2
Subham Rout <53035764+subham1rout@users.noreply.github.com>
2023-08-29 21:19:11 +0530

    Time: 69 ms (47.96%), Space: 48.6 MB (11.61%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -3,13 +3,16 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
+    let map=new Map();
     for(let i=0;i<nums.length;i++){
-        let count=0;
-        for(let j=0;j<nums.length;j++){
-            if(nums[i]==nums[j]){
-                count++;
-            }
+        let value=map.get(nums[i]);
+        if(!value){
+            map.set(nums[i],1);
+        }else{
+            map.set(nums[i],value+1);
         }
-        if(count==1) return nums[i];
+    }
+    for(const [key,value] of map){
+        if(value==1) return key;
     }
 };
