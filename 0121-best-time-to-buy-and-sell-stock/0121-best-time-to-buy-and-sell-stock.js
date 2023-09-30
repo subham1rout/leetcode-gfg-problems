dab165e4420b5df1180fef751e051ef79831394b
Subham Rout <53035764+subham1rout@users.noreply.github.com>
2023-09-30 22:48:55 +0530

    Time: 59 ms (96.60%), Space: 51.4 MB (76.68%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,16 +3,16 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let min = prices[0];
-    let profit = 0;
-    for (let i = 1; i < prices.length; i++) {
-        let cost = prices[i] - min;
-        if (cost > profit) {
-            profit = cost;
+    let maxsum=0;
+    let min=prices[0];
+    for(let i=1;i<prices.length;i++){
+        let cost=prices[i]-min;
+        if(cost>maxsum){
+            maxsum=cost;
         }
-        if (prices[i] < min) {
-            min = prices[i];
+        if(min>prices[i]){
+            min=prices[i];
         }
     }
-    return profit;
+    return maxsum;
 };
