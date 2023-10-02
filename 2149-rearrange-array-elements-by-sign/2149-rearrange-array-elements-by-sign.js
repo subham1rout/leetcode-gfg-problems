85464fed13561cdea00c8fc6ac1905858569fce4
Subham Rout <53035764+subham1rout@users.noreply.github.com>
2023-10-02 22:57:08 +0530

    Time: 331 ms (11.01%), Space: 101.5 MB (21.31%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.js
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.js
@@ -3,18 +3,17 @@
  * @return {number[]}
  */
 var rearrangeArray = function(nums) {
-    let length=nums.length;
-    let posindex=0;
-    let negindex=1;
-    let ans=new Array(length).fill(0);
-    for(let i=0;i<length;i++){
-        if(nums[i]>=0){
-            ans[posindex]=nums[i];
-            posindex+=2;
-        }else{
-            ans[negindex]=nums[i];
-            negindex+=2;
-        }
+    let parr=[];
+    let narr=[];
+    let n=nums.length;
+    for(let i=0;i<n;i++){
+        if(nums[i]<0) narr.push(nums[i]);
+        else parr.push(nums[i]);
+    }
+    let ans=[];
+    for(let i=0,j=0;i<parr.length&&j<narr.length;i++,j++){
+        ans.push(parr[i]);
+        ans.push(narr[j]);
     }
     return ans;
 };
