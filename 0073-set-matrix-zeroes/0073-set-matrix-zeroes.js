3ff4306fd8ba5bbe2bdb2ba84922d5799907506a
Subham Rout <53035764+subham1rout@users.noreply.github.com>
2023-10-15 23:17:36 +0530

    Time: 57 ms (97.03%), Space: 45.1 MB (31.99%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
@@ -3,35 +3,23 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 
-var markRow=(arr,i,n)=>{
-    for(let j=0;j<n;j++){
-        if(arr[i][j]!==0){
-            arr[i][j]=Number.MAX_SAFE_INTEGER;
-        }
-    }
-}
-var markColumn=(arr,j,m)=>{
-    for(let i=0;i<m;i++){
-        if(arr[i][j]!==0){
-            arr[i][j]=Number.MAX_SAFE_INTEGER;
-        }
-    }
-}
 var setZeroes = function(matrix) {
     let m=matrix.length;
     let n=matrix[0].length;
-    for(let i=0;i<m;i++){
-        for(let j=0;j<n;j++){
-            if(matrix[i][j]==0){
-                markRow(matrix,i,n);
-                markColumn(matrix,j,m);
+    let row = new Array(m).fill(0);
+    let col = new Array(n).fill(0);
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (matrix[i][j] == 0) {
+                row[i] = 1;
+                col[j] = 1;
             }
         }
     }
-    for(let i=0;i<m;i++){
-        for(let j=0;j<n;j++){
-            if(matrix[i][j]==Number.MAX_SAFE_INTEGER){
-                matrix[i][j]=0;
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (row[i] || col[j]) {
+                matrix[i][j] = 0;
             }
         }
     }
