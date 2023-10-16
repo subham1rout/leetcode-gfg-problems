6ce68352f7f80f456c13b4b3cdddd689de1ca2af
Subham Rout <53035764+subham1rout@users.noreply.github.com>
2023-10-17 00:45:43 +0530

    Time: 62 ms (10.33%), Space: 42.3 MB (35.61%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.js
+++ 0048-rotate-image/0048-rotate-image.js
@@ -3,23 +3,28 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var rotate = function(matrix) {
-    let m=matrix.length;
-    let newmatrix = [];
-    for (let i = 0; i < m; i++) {
-        let arr = [];
-        for (let j = 0; j < m; j++) {
-            arr.push(0);
-        }
-        newmatrix.push(arr);
-    }
-    for (let i = 0; i < m; i++) {
-        for (let j = 0; j < m; j++) {
-            newmatrix[j][m-1 - i] = matrix[i][j];
+    let n=matrix.length;
+    for(let i=0;i<n-1;i++){
+        for(let j=i+1;j<n;j++){
+            let temp=matrix[i][j];
+            matrix[i][j]=matrix[j][i];
+            matrix[j][i]=temp;
         }
     }
-    for(let i=0;i<m;i++){
-        for(let j=0;j<m;j++){
-            matrix[i][j]=newmatrix[i][j];
-        }
+    for(let i=0;i<n;i++){
+        reverse(matrix[i]);
     }
 };
+
+var reverse=(arr)=>{
+    let n=arr.length;
+    let start=0;
+    let end=n-1;
+    while(start<end){
+        let temp=arr[start];
+        arr[start]=arr[end];
+        arr[end]=temp;
+        start++;
+        end--;
+    }
+}
