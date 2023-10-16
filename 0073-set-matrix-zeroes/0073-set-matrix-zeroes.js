9ab9d0bc1d84beb137e26e12e81f5e7d99a8694f
Subham Rout <53035764+subham1rout@users.noreply.github.com>
2023-10-16 23:05:58 +0530

    Time: 64 ms (82.40%), Space: 45 MB (47.07%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
@@ -6,21 +6,35 @@
 var setZeroes = function(matrix) {
     let m=matrix.length;
     let n=matrix[0].length;
-    let row = new Array(m).fill(0);
-    let col = new Array(n).fill(0);
-    for (let i = 0; i < m; i++) {
-        for (let j = 0; j < n; j++) {
-            if (matrix[i][j] == 0) {
-                row[i] = 1;
-                col[j] = 1;
+    let col1=1;
+    for(let i=0;i<m;i++){
+        for(let j=0;j<n;j++){
+            if(matrix[i][j]==0){
+                if(j==0){
+                    col1=0;
+                    matrix[i][0]=0;
+                }else{
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
             }
         }
     }
-    for (let i = 0; i < m; i++) {
-        for (let j = 0; j < n; j++) {
-            if (row[i] || col[j]) {
-                matrix[i][j] = 0;
+    for(let i=1;i<m;i++){
+        for(let j=1;j<n;j++){
+            if(matrix[i][0]==0 || matrix[0][j]==0){
+                matrix[i][j]=0;
             }
+        }
+    }
+    for(let j=1;j<n;j++){
+        if(matrix[0][0]==0 || matrix[0][j]==0){
+            matrix[0][j]=0;
+        }
+    }
+    for(let i=0;i<m;i++){
+        if(col1==0 || matrix[i][0]==0){
+            matrix[i][0]=0;
         }
     }
 };
