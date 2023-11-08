fb83550f033d56451535042a82afb1d45cd44663
Subham Rout <53035764+subham1rout@users.noreply.github.com>
2023-11-08 13:07:30 +0530

    Time: 63 ms (9.14%), Space: 42.4 MB (31.99%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.js
+++ 0118-pascals-triangle/0118-pascals-triangle.js
@@ -3,22 +3,21 @@
  * @return {number[][]}
  */
 
-var getValue=function(r,c){
-    let value=1;
-    for(let i=0;i<c;i++){
-        value=value*(r-i);
-        value=Math.floor(value/(i+1));
+var getRow=function(n){
+    let ans=1;
+    let arr=[];
+    arr.push(ans);
+    for(let i=1;i<n;i++){
+        ans=ans*(n-i);
+        ans=Math.floor(ans/i);
+        arr.push(ans);
     }
-    return value;
+    return arr;
 }
 var generate = function(numRows) {
     let ans=[];
     for(let i=1;i<=numRows;i++){
-        let arr=[];
-        for(let j=1;j<=i;j++){
-            arr.push(getValue(i-1,j-1));
-        }
-        ans.push(arr);
+        ans.push(getRow(i));
     }
     return ans;
 };
