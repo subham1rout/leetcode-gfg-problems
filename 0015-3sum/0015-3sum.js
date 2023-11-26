f63f48fa964ca511e985175aeef9ccf833629ce0
Subham Rout <53035764+subham1rout@users.noreply.github.com>
2023-11-26 20:58:52 +0530

    Time: 139 ms (91.67%), Space: 58.8 MB (71.20%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -3,22 +3,27 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    let set = new Set();
+    let ans = [];
+    nums.sort((a, b) => a - b);
     let n=nums.length;
     for (let i = 0; i < n; i++) {
-        let tempSet = new Set();
-        for (let j = i + 1; j < n; j++) {
-            let k = -(nums[i] + nums[j]);
-            if (tempSet.has(k)) {
-                let x = [nums[i], nums[j], k].sort((a, b) => a - b);
-                set.add(JSON.stringify(x));
+        if (i > 0 && nums[i - 1] == nums[i]) continue;
+        let j = i + 1;
+        let k = n - 1;
+        while (j < k) {
+            let sum = nums[i] + nums[j] + nums[k];
+            if (sum > 0) {
+                k--;
+            } else if (sum < 0) {
+                j++;
+            } else {
+                ans.push([nums[i], nums[j], nums[k]]);
+                j++;
+                k--;
+                while (j < k && nums[j] == nums[j - 1]) j++;
+                while (j < k && nums[k] == nums[k + 1]) k--;
             }
-            tempSet.add(nums[j]);
         }
-    }
-    let ans = [];
-    for (const value of set) {
-        ans.push(JSON.parse(value));
     }
     return ans;
 };
