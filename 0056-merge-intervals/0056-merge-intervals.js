b7cdfc98fd5c810da240784dad4106b0c6202beb
Subham Rout <53035764+subham1rout@users.noreply.github.com>
2023-12-27 01:19:40 +0530

    Time: 83 ms (87.49%), Space: 48.8 MB (73.72%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.js
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -7,22 +7,13 @@ var merge = function(intervals) {
     intervals.sort((a,b)=>a[0]-b[0]);
     let ans=[];
     for(let i=0;i<n;i++){
-        let start=intervals[i][0];
-        let end=intervals[i][1];
-        if(ans.length!==0 && ans[ans.length-1][1]>=end){
-            continue;
-        }
-        for(let j=i+1;j<n;j++){
-            if(intervals[j][0]<=end){
-                if(end<intervals[j][1]){
-                    end=intervals[j][1];
-                }
-
-            }else{
-                break;
+        if(ans.length==0 || ans[ans.length-1][1]<intervals[i][0]){
+            ans.push(intervals[i]);
+        }else{
+            if(intervals[i][1] > ans[ans.length-1][1]){
+                ans[ans.length-1][1]=intervals[i][1];
             }
         }
-        ans.push([start,end]);
     }
     return ans;
 };
