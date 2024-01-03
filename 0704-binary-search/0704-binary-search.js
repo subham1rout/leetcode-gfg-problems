f38114f34fc5d3aa99c26d1bb3837c941fa91edc
Subham Rout <53035764+subham1rout@users.noreply.github.com>
2024-01-03 22:10:37 +0530

    Time: 54 ms (67.76%), Space: 44.6 MB (82.39%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -6,11 +6,12 @@
 var search = function(nums, target) {
     let low=0;
     let high=nums.length-1;
-    while(low<=high){
+    function rec(nums,target,low,high){
+        if(low>high) return -1;
         let mid=Math.floor((low+high)/2);
-        if(nums[mid]==target) return mid;
-        else if(target>nums[mid]) low=mid+1;
-        else high=mid-1;
+        if(target==nums[mid]) return mid;
+        else if(target>nums[mid]) return rec(nums,target,mid+1,high);
+        else return rec(nums,target,low,mid-1);
     }
-    return -1;
+    return rec(nums,target,low,high);
 };
