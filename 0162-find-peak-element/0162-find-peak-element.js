1be3146ddddf6b433107923450ef0d46e6975bd8
Subham Rout <53035764+subham1rout@users.noreply.github.com>
2024-02-23 23:28:45 +0530

    Time: 50 ms (69.96%), Space: 49.1 MB (14.83%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.js
+++ 0162-find-peak-element/0162-find-peak-element.js
@@ -4,19 +4,9 @@
  */
 var findPeakElement = function(nums) {
     let n=nums.length;
-    if(n==1) return 0;
-    if(nums[0]>nums[1]) return 0;
-    if(nums[n-1]>nums[n-2]) return n-1;
-    let low=1;
-    let high=n-1;
-    while(low<=high){
-        let mid=Math.floor((low+high)/2);
-        if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
-            return mid;
-        }else if(nums[mid]>nums[mid-1]){
-            low=mid+1;
-        }else{
-            high=mid-1;
+    for(let i=0;i<n;i++){
+        if((i==0 || nums[i-1]<nums[i]) && (i==n-1 || nums[i]>nums[i+1])){
+            return i;
         }
     }
 };
