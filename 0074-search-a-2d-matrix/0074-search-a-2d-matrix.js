dbfbdbc5d69ae96c36caab55d68eb2c77cdbbdf0
Subham Rout <53035764+subham1rout@users.noreply.github.com>
2024-05-07 16:13:49 +0530

    Time: 54 ms (46.13%), Space: 49.1 MB (33.23%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    let n=matrix.length;
+    let m=matrix[0].length;
+    for(let i=0;i<n;i++){
+        for(let j=0;j<m;j++){
+            if(matrix[i][j]==target){
+                return true;
+            }
+        }
+    }
+    return false;
+};
