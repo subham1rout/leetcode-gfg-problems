892031e892403fde3e2e1ed8b65dc0ff17f7e277
Subham Rout <53035764+subham1rout@users.noreply.github.com>
2024-05-07 21:46:26 +0530

    Time: 312 ms (55.41%), Space: 53.4 MB (13.09%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.js
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.js
@@ -6,20 +6,15 @@
 var searchMatrix = function(matrix, target) {
     let n=matrix.length;
     let m=matrix[0].length;
-    for(let i=0;i<n;i++){
-        if(matrix[i][0]<=target && target<=matrix[i][m-1]){
-            let low=0;
-            let high=m-1;
-            while(low<=high){
-                let mid=Math.floor((low+high)/2);
-                if(matrix[i][mid]==target){
-                    return true;
-                }else if(matrix[i][mid]<target){
-                    low=mid+1;
-                }else{
-                    high=mid-1;
-                }
-            }
+    let row=0;
+    let column=m-1;
+    while(row<n && column>=0){
+        if(matrix[row][column]==target){
+            return true;
+        }else if(matrix[row][column]<target){
+            row++;
+        }else{
+            column--;
         }
     }
     return false;
