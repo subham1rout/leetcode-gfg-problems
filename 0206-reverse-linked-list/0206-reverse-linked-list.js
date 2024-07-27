419d6ba351672b25142500373c49b3cd3abb40e8
Subham Rout <53035764+subham1rout@users.noreply.github.com>
2024-07-28 00:04:12 +0530

    Time: 51 ms (91.92%), Space: 51.7 MB (63.91%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.js
+++ 0206-reverse-linked-list/0206-reverse-linked-list.js
@@ -10,29 +10,16 @@
  * @return {ListNode}
  */
 
-class Stack{
-    constructor(){
-        this.items=[];
-    }
-    push(data){
-        this.items.push(data);
-    }
-    pop(){
-        let item=this.items.pop();
-        return item;
-    }
-}
 var reverseList = function(head) {
-    let temp=head;
-    let newstack=new Stack();
-    while(temp){
-        newstack.push(temp.val);
-        temp=temp.next;
+    let prev = null;
+    let current = head;
+    
+    while (current) {
+        let nextNode = current.next;
+        current.next = prev;        
+        prev = current;              
+        current = nextNode;
     }
-    temp=head;
-    while(temp){
-        temp.val=newstack.pop();
-        temp=temp.next;
-    }
-    return head;
+    
+    return prev;
 };
