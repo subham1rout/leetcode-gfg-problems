4480009356299be97117186c26412e52c9c8e0e4
Subham Rout <53035764+subham1rout@users.noreply.github.com>
2024-08-15 00:08:43 +0530

    Time: 54 ms (64.78%), Space: 49.1 MB (59.26%) - LeetHub

---
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[]} operations
+ * @return {number}
+ */
+var finalValueAfterOperations = function(operations) {
+    let X=0;
+    for(let i=0;i<operations.length;i++){
+        if(operations[i]=="++X" || operations[i]=="X++"){
+            X=X+1;
+        }else{
+            X=X-1;
+        }
+    }
+    return X;
+};
