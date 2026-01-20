4287c1148daf6c6606212e36278ed17f6e40062b
Subham Rout <53035764+subham1rout@users.noreply.github.com>
2026-01-20 12:39:08 +0530

    Time: 2 ms (49.20%) | Memory: 54.3 MB (95.43%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.ts
+++ 14-longest-common-prefix/longest-common-prefix.ts
@@ -2,7 +2,7 @@ function longestCommonPrefix(strs: string[]): string {
     let res="";
     for(let i=0;i<strs[0].length;i++){
         for(let j=0;j<strs.length;j++){
-            if(strs[0][i]!= strs[j][i]){
+            if(i==strs[j].length || strs[0][i]!= strs[j][i]){
                 return res;
             }
         }
